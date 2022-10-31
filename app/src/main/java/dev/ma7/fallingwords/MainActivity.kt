27d2ae261fd331055bf0213f63e96f8e2eb0d408
Milad <j2milad7@gmail.com>
2022-10-31 09:56:01 +0330

    Initialize the project
    Signed-off-by: Milad <j2milad7@gmail.com>

---
+++ app/src/main/java/dev/ma7/fallingwords/MainActivity.kt
@@ -0,0 +1,12 @@
+package dev.ma7.fallingwords
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
