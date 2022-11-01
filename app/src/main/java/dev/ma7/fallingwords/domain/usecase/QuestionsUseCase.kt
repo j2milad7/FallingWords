0de274e0a2858eefd129fec881bd14799ead9b8f
Milad <j2milad7@gmail.com>
2022-11-01 06:49:44 +0330

    Add 'QuestionUseCase' and related classes.
    Signed-off-by: Milad <j2milad7@gmail.com>

---
+++ app/src/main/java/dev/ma7/fallingwords/domain/usecase/QuestionsUseCase.kt
@@ -0,0 +1,59 @@
+package dev.ma7.fallingwords.domain.usecase
+
+import arrow.core.Either
+import dev.ma7.fallingwords.data.model.WordDto
+import dev.ma7.fallingwords.domain.model.GameDifficulty
+import dev.ma7.fallingwords.domain.model.Question
+import dev.ma7.fallingwords.domain.repository.WordsRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+import kotlin.random.Random
+
+class QuestionsUseCase @Inject constructor(
+    private val wordsRepository: WordsRepository
+) {
+
+    suspend operator fun invoke(gameDifficulty: GameDifficulty): Either<Throwable, List<Question>> {
+        return withContext(Dispatchers.IO) {
+            val response = wordsRepository.getWordList()
+            if (response.isLeft()) {
+                return@withContext response as Either.Left
+            }
+
+            return@withContext try {
+                Either.Right(
+                    generateQuestionList(gameDifficulty, wordList = response.orNull().orEmpty())
+                )
+            } catch (e: Exception) {
+                Either.Left(e)
+            }
+        }
+    }
+}
+
+private fun generateQuestionList(
+    gameDifficulty: GameDifficulty,
+    wordList: List<WordDto>
+): List<Question> {
+    if (wordList.isEmpty()) return emptyList()
+    if (wordList.size < gameDifficulty.wordsCount) {
+        throw IllegalArgumentException("This game difficulty could not be chosen at this time!")
+    }
+
+    val shuffledWordList = wordList.shuffled()
+    val gameWordList = shuffledWordList.subList(0, gameDifficulty.wordsCount)
+    val leftWorldList = shuffledWordList.subList(
+        gameDifficulty.wordsCount,
+        shuffledWordList.size
+    )
+    return gameWordList.map {
+        val shouldHaveCorrectTranslation = Random.nextBoolean()
+        val translation = when {
+            shouldHaveCorrectTranslation -> it.spanish
+            leftWorldList.isEmpty() -> gameWordList.random().spanish
+            else -> leftWorldList.random().spanish
+        }
+        Question(it.english, translation, shouldHaveCorrectTranslation)
+    }
+}
--- app/src/test/java/dev/ma7/fallingwords/util/TestData.kt
+++ app/src/test/java/dev/ma7/fallingwords/util/TestData.kt
@@ -2,6 +2,8 @@
 
 package dev.ma7.fallingwords.util
 
+import dev.ma7.fallingwords.data.model.WordDto
+
 const val WORDS_JSON = "[\n" +
         "  {\n" +
         "    \"text_eng\": \"primary school\",\n" +
@@ -12,3 +14,16 @@ const val WORDS_JSON = "[\n" +
         "    \"text_spa\": \"profesor / profesora\"\n" +
         "  }\n" +
         "]"
+
+val wordList = listOf(
+    WordDto(english = "English 1", spanish = "Spanish 1"),
+    WordDto(english = "English 2", spanish = "Spanish 2"),
+    WordDto(english = "English 3", spanish = "Spanish 3"),
+    WordDto(english = "English 4", spanish = "Spanish 4"),
+    WordDto(english = "English 5", spanish = "Spanish 5"),
+    WordDto(english = "English 6", spanish = "Spanish 6"),
+    WordDto(english = "English 7", spanish = "Spanish 7"),
+    WordDto(english = "English 8", spanish = "Spanish 8"),
+    WordDto(english = "English 9", spanish = "Spanish 9"),
+    WordDto(english = "English 10", spanish = "Spanish 10")
+)
